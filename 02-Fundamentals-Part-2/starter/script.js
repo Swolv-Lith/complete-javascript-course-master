78e3e3d620bb4087de29a77baa10594c6f3e1a00
Marcelle Nhemetz <marcelle.frujugar@gmail.com>
2020-11-26 13:24:27 -0300

    35. Arrow function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -14,4 +14,17 @@ const calcAge2 = function (birthYear) {
 }
 const age2 = calcAge2(1998)
 
-console.log(age1, age2)
+// 35. Arrow Function
+
+const calcAge3 = birthYear => 2057 - birthYear
+const age3 = calcAge3(1998)
+
+console.log(age1, age2, age3)
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2020 - birthYear
+  const retirement = 65 - age
+  return `${firstName} retires is ${retirement} years.`
+}
+
+console.log(yearsUntilRetirement(1998, 'Marshall'))
