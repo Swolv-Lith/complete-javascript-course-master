87d4d4c324a4bb0edf7415890b2ced31ff934663
Marshall <swolv.lith@gmail.com>
2021-02-26 10:02:35 -0300

    updating

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 'A vida tem momentos bons e ruins, então porque não tirar uma folga e pensar em si mesmo?!';
+
+const batata = bts => bts = bts + ' Se amar é o melhor remédio para automedicação!';
+
+console.log(batata(x));
+
+//TODO BUG VIDEO FIXME
